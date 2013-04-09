a6df8bf600de0a23c548239986cef22f712ed6f9
Kenneth Ballenegger <kenneth@ballenegger.com>
2013-04-09 14:25:44 -0700

    progress :)

--- lib/validations.rb
+++ lib/validations.rb
@@ -9,7 +9,7 @@ module Validations
     #
     def validations(&block)
       return @validations unless block
-      @validations = Validator.new(block)
+      @validations = Validator.new(&block)
     end
   end
 
@@ -30,24 +30,56 @@ module Validations
 
     def self.parse_block(&block)
       # execute block, return array of validation methods called
+      context = BlockParsingContext.new
+      context.instance_eval(&block)
+      context.validations
     end
 
-    def initialize(block)
-      @block = block
+    def initialize(&block)
+      @validations = self.class.parse_block(&block)
     end
 
     def validates?(context)
-      true
+      p @validations
     end
   end
 
-  class Context
+  private
+
+  class BlockParsingContext < BasicObject
+
+    def initialize
+      @validations = []
+    end
+
+    attr_reader :validations
+
+    def method_missing(method, *args, &block)
+      raise "Undefined validation #{method}..." unless ValidationContext.method_defined?(method)
+      opts = args.pop if args.last.is_a?(::Hash)
+      @validations << {
+        name: method,
+        args: args,
+        opts: opts,
+        block: block
+      }
+    end
+  end
+
+  class ValidationContext < BasicObject
 
     def initialize(context)
       @context = context
     end
+    
+    def context_eval(&block)
+      @context.instance_eval(&block)
+    end
 
     # def a method for each validation
+    def validates_presence_of(key)
+      @context.include?(key)
+    end
   end
 
 end
