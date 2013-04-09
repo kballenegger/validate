f68fe3078c84b4469ccd503100202badb809d669
Kenneth Ballenegger <kenneth@ballenegger.com>
2013-04-08 19:27:28 -0700

    basic framework for validations library

--- lib/validations.rb
+++ lib/validations.rb
@@ -1,5 +1,39 @@
-require "validations/version"
+require 'validations/version'
+
 
 module Validations
-  # Your code goes here...
+
+  module ClassMethods
+    # Doubles as setter & getter for @validations
+    # This method is also the main interface to this lib.
+    #
+    def validations(&block)
+      return @validations unless block
+      @validations = ValidationsImpl.new(block)
+    end
+  end
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+
+  # May throw a nil NoMethodError if validations are not defined properly.
+  #
+  def validates?
+    self.class.validations.validates?(self)
+  end
+
+
+  # Actual implementation
+  class ValidationsImpl
+
+    def initialize(block)
+      @block = block
+    end
+
+    def validates?(context)
+      true
+    end
+  end
+
 end
