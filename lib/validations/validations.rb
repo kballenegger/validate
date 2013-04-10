cf7ea093bfa19b1277770bc1074afacfd7d0b67f
Kenneth Ballenegger <kenneth@ballenegger.com>
2013-04-09 17:26:22 -0700

    document / spec out

--- lib/validations/validations.rb
+++ lib/validations/validations.rb
@@ -1,9 +1,16 @@
 
 module Validations
 
+  # Every validation method has four arguments:
+  #
+  #   obj:          the object to validate
+  #   field:        the field to validate
+  #   opts:         the options for the validation
+  #   validations:  any children validations
+  #
   module ValidationMethods
 
-    def validates_presence_of(obj, field, opts)
+    def validates_presence_of(obj, field, opts, validations)
       obj.include?(field)
     end
   end
